81bd24bdfca3cb2dbd71eefbcb2739e8fc7ceff3
tris203 <admin@snappeh.com>
2024-10-30 12:13:39 +0000

    chore: format

--- lua/rzls/utils/documentstore.lua
+++ lua/rzls/utils/documentstore.lua
@@ -4,10 +4,10 @@ local M = {}
 ---@param buffnr integer
 ---@return integer
 function M.get_virtual_lines_count(buffnr)
-  local lines = vim.api.nvim_buf_line_count(buffnr)
-  --NOTE: Remove version line
-  lines = lines - 1
-  return lines
+    local lines = vim.api.nvim_buf_line_count(buffnr)
+    --NOTE: Remove version line
+    lines = lines - 1
+    return lines
 end
 
 return M
