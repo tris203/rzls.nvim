05be61077896dc1716f90ce8921003bcebc8eea5
tris203 <admin@snappeh.com>
2024-10-30 21:11:06 +0000

    tests: uuid tests

--- lua/rzls/utils/init.lua
+++ lua/rzls/utils/init.lua
@@ -23,12 +23,11 @@ function M.buffer_eol(bufnr)
     return eols[vim.bo[bufnr].fileformat]
 end
 
+math.randomseed(tonumber(tostring(os.time()):reverse():sub(1, 9)))
 function M.uuid()
-    local random = math.randomseed(tonumber(tostring(os.time()):reverse():sub(1, 9)))
-    assert(random, "math.randomseed failed")
     local template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
     return string.gsub(template, "[xy]", function(c)
-        local v = (c == "x") and random(0, 0xf) or random(8, 0xb)
+        local v = (c == "x") and math.random(0, 0xf) or math.random(8, 0xb)
         return string.format("%x", v)
     end)
 end
