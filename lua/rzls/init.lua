a62053dba36faf84340b22fef2d71562649770c4
Tristan Knight <admin@snappeh.com>
2024-11-01 16:08:46 +0000

    feat: support mason installation (#8)

--- lua/rzls/init.lua
+++ lua/rzls/init.lua
@@ -6,7 +6,24 @@ local M = {}
 ---@class rzls.Config
 ---@field on_attach function
 ---@field capabilities table
----@field path string
+---@field path string?
+
+--- return the path to the rzls executable
+---@param config rzls.Config
+---@return string
+local function get_cmd_path(config)
+    ---@return string
+    local function get_mason_installation()
+        local mason_installation = vim.fs.joinpath(vim.fn.stdpath("data") --[[@as string]], "mason", "bin", "rzls")
+        return vim.uv.os_uname().sysname == "Windows_NT" and string.format("%s.cmd", mason_installation)
+            or mason_installation
+    end
+
+    if config.path then
+        return config.path
+    end
+    return get_mason_installation()
+end
 
 ---@type rzls.Config
 local defaultConfg = {
@@ -14,7 +31,6 @@ local defaultConfg = {
         return nil
     end,
     capabilities = vim.lsp.protocol.make_client_capabilities(),
-    path = "/tmp",
 }
 
 ---@type lsp.ClientCapabilities
@@ -22,8 +38,10 @@ local extraCapabilities = {
     colorProvider = true,
 }
 
+---@param config rzls.Config
 function M.setup(config)
     local rzlsconfig = vim.tbl_deep_extend("force", defaultConfg, config)
+    rzlsconfig.path = rzlsconfig.path or get_cmd_path(rzlsconfig)
     vim.filetype.add({
         extension = {
             razor = "razor",
