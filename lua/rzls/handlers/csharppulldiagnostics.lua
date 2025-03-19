c83af8ae4e267cddf67f5113546adc1c096de7db
tris203 <admin@snappeh.com>
2025-03-19 21:43:15 +0000

    feat!: update C# pull diagnostics params
    Refactor the C# pull diagnostics handler to use a new identifier format.
    Update diagnostic parameters to extract the URI and version from the new
    identifier field. Also add type annotations for
    CSharpPullDiagnosticParams in the Razor module.
    
    Support for rzls 9.0.0-preview.25161.2

--- lua/rzls/handlers/csharppulldiagnostics.lua
+++ lua/rzls/handlers/csharppulldiagnostics.lua
@@ -4,22 +4,25 @@ local razor = require("rzls.razor")
 local empty_response = {}
 
 ---@param _err lsp.ResponseError
----@param result lsp.DocumentDiagnosticParams
+---@param result razor.CSharpPullDiagnosticParams
 ---@param _ctx lsp.HandlerContext
 ---@param _config table
 return function(_err, result, _ctx, _config)
-    local virtual_document =
-        documentstore.get_virtual_document(result.textDocument.uri, razor.language_kinds.csharp, "any")
+    local virtual_document = documentstore.get_virtual_document(
+        result.identifier.textDocumentIdentifier.uri,
+        razor.language_kinds.csharp,
+        result.identifier.version
+    )
     if not virtual_document then
         return empty_response
     end
 
     ---@type lsp.DocumentDiagnosticParams
-    local diagnostic_params = vim.tbl_deep_extend("force", result, {
+    local diagnostic_params = {
         textDocument = {
             uri = virtual_document.uri,
         },
-    })
+    }
 
     ---@type lsp.Diagnostic[]
     local diagnostic_response =
